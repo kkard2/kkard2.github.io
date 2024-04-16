78b368daa8606eb7966609d0e74f6fa6fbc77ce1
kkard2 <kkard2.acct@gmail.com>
2024-04-16 21:42:22 +0200

    yes, this is a start of parsing html, why do you ask

--- deploy.py
+++ deploy.py
@@ -1,7 +1,8 @@
 import os
 import shutil
 
-def process(src_file, dest_dir, start_dir, template_file):
+
+def process_templating(src_file, dest_dir, start_dir, template_file):
     relative_path = os.path.relpath(src_file, start_dir)
     dest_file = os.path.join(dest_dir, relative_path)
 
@@ -25,7 +26,8 @@ def process(src_file, dest_dir, start_dir, template_file):
     with open(src_file, 'r') as source:
         for line in source:
             if '<!-- #include' in line:
-                include_file = line.split('<!-- #include')[1].split('-->')[0].strip()
+                include_file = line.split('<!-- #include')[1]
+                include_file = include_file.split('-->')[0].strip()
                 include_path = os.path.join(start_dir, include_file)
                 with open(include_path, 'r') as include_content:
                     content += include_content.read()
@@ -40,9 +42,43 @@ def process(src_file, dest_dir, start_dir, template_file):
                 if '##content##' in template_line:
                     destination.write(content)
                 elif '##title##' in template_line:
-                    destination.write(template_line.replace('##title##', title))
+                    destination.write(
+                            template_line.replace('##title##', title))
                 else:
                     destination.write(template_line)
+
+
+def process_autolinks(dest_file):
+    file_extension = src_file.split('.')[-1]
+
+    if file_extension != 'html':
+        return
+
+    content = ''
+    tags = ['h1', 'h2', 'h3']
+    closing = False
+
+    with open(dest_file, 'r') as source:
+        for line in source:
+            for tag in tags:
+                # only accepts headings witout styling
+                if '<' + tag + '>' in line:
+                    if closing:
+                        print("sth no yes in " + dest_file + " line " + line)
+                        return
+
+
+            # if '<!-- #include' in line:
+            #     include_file = line.split('<!-- #include')[1]
+            #     include_file = include_file.split('-->')[0].strip()
+            #     include_path = os.path.join(start_dir, include_file)
+            #     with open(include_path, 'r') as include_content:
+            #         content += include_content.read()
+            # elif '<!-- #title' in line:
+            #     title = line.split('<!-- #title')[1].split('-->')[0].strip()
+            # else:
+            #     content += line
+
 
 
 start_dir = "./src"
@@ -55,4 +91,4 @@ if os.path.exists(dest_dir):
 for root, dirs, files in os.walk(start_dir):
     for file in files:
         src_file = os.path.join(root, file)
-        process(src_file, dest_dir, start_dir, template_file)
+        process_templating(src_file, dest_dir, start_dir, template_file)
