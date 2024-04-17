762766cec7b75e0aa29ea86df1ed633a53bea3e4
kkard2 <kkard2.acct@gmail.com>
2024-04-17 13:40:16 +0200

    parsing.

--- deploy.py
+++ deploy.py
@@ -49,36 +49,47 @@ def process_templating(src_file, dest_dir, start_dir, template_file):
 
 
 def process_autolinks(dest_file):
-    file_extension = src_file.split('.')[-1]
+    file_extension = dest_file.split('.')[-1]
 
     if file_extension != 'html':
         return
 
     content = ''
     tags = ['h1', 'h2', 'h3']
-    closing = False
 
     with open(dest_file, 'r') as source:
         for line in source:
             for tag in tags:
+                linestr = line.strip()
+                tagb = '<' + tag + '>'
+                tage = '</' + tag + '>'
+
                 # only accepts headings witout styling
-                if '<' + tag + '>' in line:
-                    if closing:
-                        print("sth no yes in " + dest_file + " line " + line)
-                        return
+                if linestr.startswith(tagb) and linestr.endswith(tage):
+                    line_content = linestr.split(tagb)[1].split(tage)[0]
+                    new_line_content = ''
 
+                    for c in line_content:
+                        if c.isalnum():
+                            new_line_content += c
+                        else:
+                            new_line_content += '-'
 
-            # if '<!-- #include' in line:
-            #     include_file = line.split('<!-- #include')[1]
-            #     include_file = include_file.split('-->')[0].strip()
-            #     include_path = os.path.join(start_dir, include_file)
-            #     with open(include_path, 'r') as include_content:
-            #         content += include_content.read()
-            # elif '<!-- #title' in line:
-            #     title = line.split('<!-- #title')[1].split('-->')[0].strip()
-            # else:
-            #     content += line
+                    line_content_final = '<a class="id-link" id="'
+                    line_content_final += new_line_content
+                    line_content_final += '" href="#'
+                    line_content_final += new_line_content
+                    line_content_final += '">'
+                    line_content_final += linestr
+                    line_content_final += '</a>\n'
 
+                    content += line_content_final
+                    break
+            else:
+                content += line
+
+    with open(dest_file, 'w') as destination:
+        destination.write(content)
 
 
 start_dir = "./src"
@@ -88,7 +99,15 @@ template_file = "./template.html"
 if os.path.exists(dest_dir):
     shutil.rmtree(dest_dir)
 
+print('process_templating...')
+
 for root, dirs, files in os.walk(start_dir):
     for file in files:
         src_file = os.path.join(root, file)
         process_templating(src_file, dest_dir, start_dir, template_file)
+
+print('process_autolinks...')
+for root, dirs, files in os.walk(dest_dir):
+    for file in files:
+        dest_file = os.path.join(root, file)
+        process_autolinks(dest_file)
